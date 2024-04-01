55d1bf6b89f854fb68c588a82a1ef2d240854d64
riyadhmobin <riyadhmobin@gmail.com>
2024-04-01 14:58:56 +0600

    leads show

--- laravel-app/resources/views/leads/manage_leads.blade.php
+++ laravel-app/resources/views/leads/manage_leads.blade.php
@@ -2,29 +2,32 @@
 
 @section('dynamic_page')
 <div class="row">
-	<div class="col-12 col-lg-8 col-xxl-9 d-flex">
+	<div class="">
 		<div class="card flex-fill">
 			<div class="card-header">
-				<h5 class="card-title mb-0">Leads</h5>
+				<h3>All Leads</h3>
 			</div>
 			<table class="table table-hover my-0">
 				<thead>
 					<tr>
 						<th>Name</th>
-						<th class="d-none d-xl-table-cell">Start Date</th>
-						<th class="d-none d-xl-table-cell">End Date</th>
-						<th>Status</th>
-						<th class="d-none d-md-table-cell">Assignee</th>
+						<th>Company Name</th>
+						<th>Email</th>
+						<th>Phone</th>
+                        <th>Lead Status</th>
+                        <th>Lead Source</th>
+						<th>Action</th>
 					</tr>
 				</thead>
 				<tbody>
                     @foreach($leads as $single)
                     <tr>
-						<td>Alexander</td>
-                        <td>mail.com</td>
-                        <td>This is something</td>
-                        <td><span class="badge bg-danger">Cancelled</span></td>
-                        <td>Something</td>
+						<td>{{ $single->first_name }} {{ $single->last_name }}</td>
+                        <td>{{ $single->company_name }}</td>
+                        <td>{{ $single->email }}</td>
+                        <td>{{ $single->phone }}</td>
+                        <td>{{ $single->lead_status }}</td>
+                        <td>{{ $single->lead_source }}</td>
                         <td>
                             <a href="" class="btn btn-primary btn-sm"><i class="align-middle" data-feather="edit"></i><span class="align-middle"></span></a>
                             <a href="" class="btn btn-danger btn-sm"><i class="align-middle" data-feather="trash-2"></i> <span class="align-middle"></span></a>
